460a4956d1279be49a890bcec7d1781e28ae15cf
Mariya Yeremenko <52876055+mashaquack@users.noreply.github.com>
2019-08-10 17:36:04 +0300

    Задание 7
    Пользователь вводит целое число. Программа выводит все целые числа, на которое
    заданное число делиться без остатка.

---
+++ Задание 7.cpp
@@ -0,0 +1,21 @@
+﻿// Задание 7.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// Пользователь вводит целое число. Необходимо вывести все целые числа, на которое
+//заданное число делиться без остатка.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int userNumber;
+	cout << "Введите любое целое число\n";
+	cin >> userNumber;
+	for ( int i = 1; i <=userNumber; i++) {
+		if (userNumber%i == 0) {
+			cout << i<<" ";
+		}
+	}
+}
+
+
