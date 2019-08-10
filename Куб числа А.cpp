5771c15acdffcdac106b7b4c48c3d7dd4994362d
Mariya Yeremenko <52876055+mashaquack@users.noreply.github.com>
2019-08-10 16:12:52 +0300

    Задание 6
    Пользователь вводит целое число А. Программа должна определить, что куб суммы
    цифр этого числа равен А*А

---
+++ Куб числа А.cpp
@@ -0,0 +1,23 @@
+﻿// Куб числа А.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <iostream>
+	using namespace std;
+
+	int main()
+	{
+		int A, A1, sum = 0;
+		cout << "A="; cin >> A;
+		if (A < 0) A = -A;
+		A1 = A;
+		while (A > 0)
+		{
+			sum += A % 10;
+			A /= 10;
+		}
+		if (sum*sum*sum == A1 * A1) cout << "YES\n";
+		else cout << "NO\n";
+		system("pause");
+		return 0;
+	}
+
+
