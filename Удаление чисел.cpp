171c19e76be847e14c0d5ce389cec67a57ffe63d
Mariya Yeremenko <52876055+mashaquack@users.noreply.github.com>
2019-08-10 14:07:59 +0300

    Код удаления заданных цифр из числа
    Пользователь вводит любое число в консоли. Программа удаляет из числа цифры 3 и 6 и выводит число на экран.

---
+++ Удаление чисел.cpp
@@ -0,0 +1,23 @@
+﻿
+#include<iostream>
+#include <vector>
+using namespace std;
+void main() {
+	setlocale(LC_ALL, "rus"); long long int a, c = 0;
+	vector <int> mas;
+	cout << "Введите число "; cin >> a;
+
+	for (int i = 0; a != 0; i++)
+	{
+		mas.push_back(a % 10); a /= 10; c++;
+	} cout << endl;
+	for (int i = c - 1; i >= 0; i--)
+		if (mas[i] != 3&& mas[i]!=6) cout << mas[i]; cout << endl;
+	
+}
+
+
+
+
+
+
