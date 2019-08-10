f48648c569123026cbab349009bd65080041ba8c
Mariya Yeremenko <52876055+mashaquack@users.noreply.github.com>
2019-08-10 14:58:31 +0300

    Add files via upload
    Пользователь вводит любое целое число А. Программа выводит все целые числа В,
    для которых А делиться без остатка на В*В и не делиться без остатка на В*В*В

---
+++ Вывод числа B.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "ru");
+	int A = 0;
+	cout << "Введите любое целое чило А: ";
+	cin >> A;
+	cout << "\n\n";
+	for (int B = 1; B < a; B++)
+	{
+		if ((A % (B * B) == 0) && (A % (B * B * B) != 0))
+			cout << B << " ";
+	}
+}
+
